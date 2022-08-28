118a37ba4293055ebec96589674d25c42c7f5df5
Sandra Yun <sandramayla@gmail.com>
2022-08-28 02:28:54 -0400

    commit tree 1165cf9e4cdb230480aafa97cf7d0ee9850c90ee

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,20 @@
 import React from "react";
+// import React, { useState } from 'react';
 
-function CategoryFilter() {
+function CategoryFilter({ categories, filterCats, catState }) {
+  
+  function handleClick(event) {
+    //console.log(event);
+    //setSelected(e.target.value)
+    filterCats(event.target.value)
+  }
+  
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {categories.map(cat => {
+        return <button className={(catState === cat) ? 'selected' : ''} onClick={handleClick} key={cat} value={cat}>{cat}</button>
+      })}
     </div>
   );
 }
--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,19 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
-function NewTaskForm() {
+function NewTaskForm({categories, onTaskFormSubmit}) {
+  const [formData, updateForm] = useState({text: '', category: 'Code'})
+  
+  function handleChange(event) {
+    const name = event.target.name
+    const updatedObj = {
+      ...formData,
+      [name]: event.target.value
+    }
+    updateForm(updatedObj)
+  }
+
   return (
-    <form className="new-task-form">
+    <form onSubmit={(event) => {
+      event.preventDefault()
+      const newTask = {...formData}
+
+      onTaskFormSubmit(newTask)
+    }} className="new-task-form">
       <label>
         Details
-        <input type="text" name="text" />
+        <input 
+        onChange={handleChange} 
+        type="text" 
+        name="text" 
+        />
       </label>
       <label>
         Category
-        <select name="category">
-          {/* render <option> elements for each category here */}
+        <select 
+        onChange={handleChange} 
+        name="category"
+        >
+          {categories.map(cat => {
+            if(cat != 'All') {
+              return <option value={cat} key={cat}>{cat}</option>
+            }
+          })}
         </select>
       </label>
-      <input type="submit" value="Add task" />
+      <input 
+      type="submit" 
+      value="Add task" 
+      />
     </form>
   );
 }
